2417330f598fc3243cc62dc18288d9ab579bf12f
Ivan Zelenin <zeleninivans@gmail.com>
2021-10-29 17:58:56 +0300

    Add img input and output

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = Image.open('input.jpg')
+img = Image.open(r'input.jpg')
 
+img.save('result.png')
+
+print('Execution is finished')
