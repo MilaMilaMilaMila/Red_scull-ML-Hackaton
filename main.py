ed9beb351ff24e4e7d5be9426473e8cefd2b9183
Ivan Zelenin <zeleninivans@gmail.com>
2021-10-29 18:22:14 +0300

    Add contour filter

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from PIL import Image
+from PIL import Image, ImageFilter
 import numpy as np
 import matplotlib.pyplot as plt
 
 img = Image.open(r'input.jpg')
 
-img.save('result.png')
+res = img.filter(ImageFilter.CONTOUR)
+
+res.save('result.png')
 
 print('Execution is finished')
