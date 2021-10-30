37754ac0423fdfe0c6936707fc8c89f4dd853eae
inter-malchik <ipad3007@icloud.com>
2021-10-30 11:54:16 +0300

    new functionality in cutting dataset images

--- cut_images.py
+++ cut_images.py
@@ -1,13 +1,46 @@
-import csv, json
+import csv
+import json
 from PIL import Image
 
 
 # before start, you should create two directories
 # train/ (with photos from the drone)
-# cutted/ (directory for the result)
+# cutted/ (directory for the positive result)
+# leaved/ (directory for the negative result)
 class HeapOfGarbage:
     def __init__(self, minx, maxx, miny, maxy, width, height):
         self.coordinates = (minx * width, miny * height, maxx * width, maxy * height)
+
+    def __contains__(self, item):
+        if item[2] > self.coordinates[0] > item[0] and item[3] > self.coordinates[1] > item[1]:
+            return True
+        if self.coordinates[2] > item[0] > self.coordinates[0] and self.coordinates[3] > item[1] > self.coordinates[1]:
+            return True
+        if item[2] > self.coordinates[0] and self.coordinates[1] > item[1] > self.coordinates[3] > item[3]:
+            return True
+        if item[0] > self.coordinates[0] > self.coordinates[2] > item[2] and \
+                self.coordinates[1] < item[1] < self.coordinates[3] < item[3]:
+            return True
+        if item[1] > self.coordinates[1] > self.coordinates[3] > item[3] and \
+                self.coordinates[0] < item[0] < self.coordinates[1] < item[1]:
+            return True
+        return False
+
+
+def cut_rest(filename, coords: list):
+    im = Image.open(f"train/{filename}")
+    sizex, sizey = im.size
+    heap = HeapOfGarbage(*coords, sizex, sizey)
+    counter = 1
+    step = 800
+    for maxi_x in range(step, sizex, step):
+        for maxi_y in range(step, sizey, step):
+            mini_x, mini_y = maxi_x - step, maxi_y - step
+            if (mini_x, mini_y, maxi_x, maxi_y) not in heap:
+                cropped_im = im.crop((mini_x, mini_y, maxi_x, maxi_y))
+                cropped_im.save(f"leaved/{filename.split('.')[0]}-CUT_terrace-{counter}.{filename.split('.')[1]}")
+                counter += 1
+    im.close()
 
 
 def cut_garbage(filename, coords: list, number_of_heap):
@@ -16,6 +49,7 @@ def cut_garbage(filename, coords: list, number_of_heap):
     cropped_im = im.crop(heap.coordinates)
     name, ext = filename.split('.')
     cropped_im.save(f"cutted/{name}-CUT_garbage-{number_of_heap + 1}.{ext}")
+    im.close()
 
 
 with open('train-all-rectangles.csv', 'r', newline='') as csvfile:
@@ -27,3 +61,5 @@ with open('train-all-rectangles.csv', 'r', newline='') as csvfile:
         data = json.loads(img[2])
         for n, heap in enumerate(data['points']):
             cut_garbage(get_image_address(img[1]), heap, n)
+        if len(data['points']) == 1:
+            cut_rest(get_image_address(img[1]), data['points'][0])
